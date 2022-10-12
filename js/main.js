bea54e96ac075cf4863cb160ca183744d08535d2
vvmaddv <115622435+vvmaddv@users.noreply.github.com>
2022-10-12 12:48:22 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+    $(".testimonial-slider").slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+    });
+
+    // mobile menu
+    $(".mobile-burger").click(function () {
+        $(".mobile-burger span").toggleClass("active")
+        $(".menu").toggleClass("open")
+        $(".nav").toggleClass("open")
+        $(".works").toggleClass("active")
+        if ($(".header-bottom").hasClass("fixed") && $(".nav").hasClass("open")) {
+            $("body").addClass("overlay")
+        } else {
+            $("body").removeClass("overlay")
+        }
+    })
+
+    
+
+    let headerBottom = $(".header-bottom")
+    $(document).scroll(function () {
+        if ($(".header-bottom").hasClass("fixed") && $(".nav").hasClass("open")) {
+            $("body").addClass("overlay")
+        } else {
+            $("body").removeClass("overlay")
+        }
+        
+        if ($(this).scrollTop() > 782) {
+            headerBottom.addClass("fixed")
+            if (headerBottom.hasClass("fixed")) {}
+        } else {
+            headerBottom.removeClass("fixed")
+        }
+    })
+
+});
